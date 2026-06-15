5200b825218ae168885d0ca188c73246f67d2303
nathblair1 <nathblair1@icloud.com>
2026-06-15 15:26:50 +0100

    Update LoginFormContainer.tsx

--- resources/scripts/components/auth/LoginFormContainer.tsx
+++ resources/scripts/components/auth/LoginFormContainer.tsx
@@ -44,11 +44,11 @@ export default forwardRef<HTMLFormElement, Props>(({ title, ...props }, ref) => 
             &copy; 2015 - {new Date().getFullYear()}&nbsp;
             <a
                 rel={'noopener nofollow noreferrer'}
-                href={'https://pterodactyl.io'}
+                href={'https://sagahosting.com'}
                 target={'_blank'}
                 css={tw`no-underline text-neutral-500 hover:text-neutral-300`}
             >
-                Pterodactyl Software
+                SagaHosting Panel
             </a>
         </p>
     </Container>
